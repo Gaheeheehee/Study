842ce0645c77159652ac650a031844a906ddfab3
CJH <excelsiorcjh@gmail.com>
2018-01-09 13:47:21 +0900

    함수 기반 뷰(FBV)의 4가지 패턴

--- Django/askdjango/dojo/urls.py
+++ Django/askdjango/dojo/urls.py
@@ -1,3 +1,4 @@
+# dojo/urls.py
 from django.urls import path, re_path
 from . import views
 
@@ -7,8 +8,12 @@ urlpatterns = [
     # re_path(r'^sum/(?P<x>\d+)/(?P<y>\d+)/(?P<z>\d+)/$', views.mysum),
     re_path(r'^sum/(?P<numbers>[\d/]+)/$', views.mysum),
     # re_path(r'^hello/(?P<name>[ㄱ-힣]+)/(?P<age>\d+)/$', views.hello)
-    path('hello/<str:name>/<int:age>/', views.hello) # django2.0의 path 설정
+    path('hello/<str:name>/<int:age>/', views.hello), # django2.0의 path 설정
     # path('sum/<int:x>/', views.mysum),  # django2.0의 path 설정
     # path('sum/<int:x>/<int:y>/', views.mysum),  # django2.0의 path 설정
     # path('sum/<int:x>/<int:y>/<int:z>/', views.mysum),  # django2.0의 path 설정
+    path('list1/', views.post_list1),
+    path('list2/', views.post_list2),
+    path('list3/', views.post_list3),
+    path('excel/', views.excel_download),
 ]
--- Django/askdjango/dojo/views.py
+++ Django/askdjango/dojo/views.py
@@ -1,5 +1,7 @@
+import os
+from django.conf import settings
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 
@@ -13,3 +15,43 @@ def mysum(request, numbers):
 
 def hello(request, name, age):
     return HttpResponse('안녕하세요. {}. {}살이시네요'.format(name, age))
+
+
+def post_list1(request):
+    '''FBV(Function Based View): 직접 문자열로 HTML 형식 응답하기'''
+
+    name = '최종현'
+    return HttpResponse('''
+            <h1>AskDjango</h1>
+            <p>{name}</p>
+            <p>Django 공부 열심히 할게요</p>
+            '''.format(name=name))
+
+
+def post_list2(request):
+    '''FBV2: 템플릿을 통해 HTML형식 응답하기'''
+
+    name = '최종현'
+    return render(request, 'dojo/post_list.html', {'name': name})
+
+
+def post_list3(request):
+    '''FBV3: JSON 형식 응답하기'''
+
+    return JsonResponse({
+        'message': '안녕 파이썬 & 장고',
+        'itmes': ['파이썬', '장고', 'Celery', 'Azure', 'AWS'],
+    },json_dumps_params={'ensure_ascii': False})
+
+
+def excel_download(request):
+    '''FBV4: Excel 다운로드 응답하기'''
+
+    # filepath = 'D:/Users/cjh/dev/Study/Study/Django/askdjango/test.xlsx'
+    filepath = os.path.join(settings.BASE_DIR, 'test.xlsx')
+    filename = os.path.basename(filepath) # 파일명 가져오기
+    with open(filepath, 'rb') as f:
+        response = HttpResponse(f, content_type='application/vnd.ms-excel')
+        # 필요한 응답헤더 세팅
+        response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
+        return response
