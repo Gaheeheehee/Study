2ab90f762969e8e353364bd727b46d729ae8ced9
CJH <excelsiorcjh@gmail.com>
2018-01-31 13:04:41 +0900

    각 app 마다 namespace를 적용

--- Django/askdjango/blog/urls.py
+++ Django/askdjango/blog/urls.py
@@ -2,6 +2,9 @@
 from django.urls import path
 from . import views
 
+# django2.0 부터는 namespace 지정 후 
+# 해당 app/urls.py에 app_name을 명시 해줘야함
+app_name = 'blog'
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('detail/<int:id>/', views.post_detail, name='post_detail')
--- Django/askdjango/dojo/urls.py
+++ Django/askdjango/dojo/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, re_path
 from . import views
 from . import views_cbv  # Class Based View import
 
+app_name = 'dojo'
 urlpatterns = [
     # re_path(r'^sum/(?P<x>\d+)/$', views.mysum),
     # re_path(r'^sum/(?P<x>\d+)/(?P<y>\d+)/$', views.mysum),
