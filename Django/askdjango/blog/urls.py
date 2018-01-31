551da9e4281dc9069a3a2108f0c92fe6d00c8448
CJH <excelsiorcjh@gmail.com>
2018-01-31 19:39:01 +0900

    CBV를 이용한 get_absolute_url 예시

--- Django/askdjango/blog/urls.py
+++ Django/askdjango/blog/urls.py
@@ -1,11 +1,14 @@
 # blog/urls.py
 from django.urls import path
 from . import views
+from . import views_cbv
 
 # django2.0 부터는 namespace 지정 후 
 # 해당 app/urls.py에 app_name을 명시 해줘야함
 app_name = 'blog'
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('detail/<int:id>/', views.post_detail, name='post_detail')
+    path('detail/<int:id>/', views.post_detail, name='post_detail'),
+
+    path('cbv/new/', views_cbv.post_new)
 ]
