40ffb1554b0e120e065cfa6c67010679436d5fdf
CJh <excelsiorcjh@gmail.com>
2018-02-07 23:40:26 +0900

    blog.views.post_new/post_edit 뷰를 구현

--- Django/askdjango/blog/views.py
+++ Django/askdjango/blog/views.py
@@ -1,7 +1,8 @@
 # blog/views.py
 from django.http import Http404
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
 from .models import Post
+from .forms import PostForm
 
 def post_list(request):
     qs = Post.objects.all()  # 이 시점에는 DB 액세스가 이루어 지지 않음 
@@ -26,4 +27,32 @@ def post_detail(request, id):
 
     return render(request, 'blog/post_detail.html', {
         'post': post,
+    })
+
+
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save()
+            return redirect(post)  # post.get_absolute_url() => post_detail
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_form.html', {
+        'form': form,
+    })
+
+
+def post_edit(request, id):
+    post = get_object_or_404(Post, id=id)
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            post = form.save()
+            return redirect(post)  # post.get_absolute_url() => post_detail
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_form.html', {
+        'form': form,
     })
