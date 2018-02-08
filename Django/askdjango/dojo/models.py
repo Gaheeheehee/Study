aa2eb47d69a06c0e5fb897af7e943bc5319e13ef
CJH <excelsiorcjh@gmail.com>
2018-02-08 18:39:24 +0900

    dojo.models.Post 모델에 user_agent 필드 추가

--- Django/askdjango/dojo/models.py
+++ Django/askdjango/dojo/models.py
@@ -1,4 +1,4 @@
-# dogo/models.py
+# dojo/models.py
 from django import forms
 from django.core.validators import MinLengthValidator
 from django.db import models
@@ -12,6 +12,7 @@ def min_length_3_validator(value):
 class Post(models.Model):
     title = models.CharField(max_length=100, validators=[min_length_3_validator])
     content = models.TextField()
+    user_agent = models.CharField(max_length=200)
     ip = models.CharField(max_length=15)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
