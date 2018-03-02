082bb4633b5622c3a38a0137525dc134615cb111
CJH <excelsiorcjh@gmail.com>
2018-03-02 13:44:44 +0900

    네이버맵 커스텀 위젯 구현

--- Django/askdjango/blog/forms.py
+++ Django/askdjango/blog/forms.py
@@ -4,9 +4,11 @@ from .models import Post
 
 
 class PostForm(forms.ModelForm):
+    # dummy = forms.CharField(widget=NaverMapPointWidget(attrs={'width': "100%", 'height': 200}))
+    
     class Meta:
         model = Post
         fields = '__all__'
         widgets = {
-            'lnglat': NaverMapPointWidget,
+            'lnglat': NaverMapPointWidget(attrs={'width': 600, 'height': 300}),
         }
