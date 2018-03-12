cd051ee45341f32900a9bea425f7ff18544edfcc
CJH <excelsiorcjh@gmail.com>
2018-03-12 12:30:13 +0900

    CBV : CreateView/UpdateView 구현

--- Django/askdjango/blog/urls.py
+++ Django/askdjango/blog/urls.py
@@ -14,5 +14,6 @@ urlpatterns = [
     path('<int:id>/edit/', views.post_edit, name='post_edit'),
 
     path('cbv/new/', views_cbv.post_new),
-    path('cbv/blog/', views_cbv.post_list2, name='post_list2'),
+    path('cbv/<int:pk>/edit/', views_cbv.post_edit),
+    # path('cbv/blog/', views_cbv.post_list2, name='post_list2'),
 ]
--- Django/askdjango/blog/views_cbv.py
+++ Django/askdjango/blog/views_cbv.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.views.generic import CreateView, ListView, DetailView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView
 from .models import Post
 
 
@@ -7,26 +7,30 @@ post_list = ListView.as_view(model=Post, paginate_by=10)
 
 post_detail = DetailView.as_view(model=Post)
 
+post_new = CreateView.as_view(model=Post)
+
+post_edit = UpdateView.as_view(model=Post, fields='__all__')
+
 # blog/forms.py
-class PostForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = '__all__'
+# class PostForm(forms.ModelForm):
+#     class Meta:
+#         model = Post
+#         fields = '__all__'
 
-class PostCreateView(CreateView):
-    model = Post
-    form_class = PostForm
-
-
-post_new = PostCreateView.as_view()
+# class PostCreateView(CreateView):
+#     model = Post
+#     form_class = PostForm
 
 
-class PostListView(ListView):
-    model = Post
-    paginate_by = 10
-
-    def get_template_names(self):
-        return 'blog/post_list2.html'
+# post_new = PostCreateView.as_view()
 
 
-post_list2 = PostListView.as_view()
+# class PostListView(ListView):
+#     model = Post
+#     paginate_by = 10
+
+#     def get_template_names(self):
+#         return 'blog/post_list2.html'
+
+
+# post_list2 = PostListView.as_view()
